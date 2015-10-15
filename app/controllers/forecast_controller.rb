8a8dfaa8ba40825a321e4682f8efc446022a1218
rradkoff <rradkoff@gmail.com>
2015-10-15 09:50:17 -0500

    Forecast Code
    Added code to enable summary of weather forecast for different
    parameters to be shown from entering in longitude and latitude data.

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -16,17 +16,26 @@ class ForecastController < ApplicationController
     # The longitude the user input is in the string @lng.
     # ==========================================================================
 
+    url_safe_lat = URI.encode(@lat)
+    url_safe_lng = URI.encode(@lng)
 
+    url = "https://api.forecast.io/forecast/714531fe02b2f727074906aa78836c12/#{@lat},#{@lng}"
+    raw_data = open(url).read
+    parsed_data = JSON.parse(open(url).read)
+    currently = parsed_data["currently"]
+    minutely = parsed_data["minutely"]
+    hourly = parsed_data["hourly"]
+    daily = parsed_data["daily"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_temperature = currently["temperature"]
 
-    @current_summary = "Replace this string with your answer."
+    @current_summary = currently["summary"]
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    @summary_of_next_sixty_minutes = minutely["summary"]
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    @summary_of_next_several_hours = hourly["summary"]
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    @summary_of_next_several_days = daily["summary"]
 
     render("coords_to_weather.html.erb")
   end
